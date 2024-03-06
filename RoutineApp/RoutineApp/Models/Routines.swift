ca6609218a9d0aacd6ae9945bb96c653652cb5ba
arthurkeusch <113102000+arthurkeusch@users.noreply.github.com>
2024-03-06 16:34:54 +0100

    Delete RoutineApp directory

--- RoutineApp/RoutineApp/Models/Routines.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Routines.swift
-//  RoutineApp
-//
-//  Created by keusch arthur on 06/03/2024.
-//
-
-import Foundation
--- RoutineApp/RoutineApp/ViewModels/RoutinesViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  RoutinesViewModel.swift
-//  RoutineApp
-//
-//  Created by keusch arthur on 06/03/2024.
-//
-
-import Foundation
