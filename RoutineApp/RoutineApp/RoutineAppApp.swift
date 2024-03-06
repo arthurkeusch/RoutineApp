49f2310752214016a413d09299dc69d536d121e8
arthurkeusch <113102000+arthurkeusch@users.noreply.github.com>
2024-03-06 16:18:10 +0100

    Init

---
+++ RoutineApp/RoutineApp/RoutineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineAppApp.swift
+//  RoutineApp
+//
+//  Created by keusch arthur on 06/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RoutineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
